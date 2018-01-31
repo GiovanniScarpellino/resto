f6ba8bf33af7af9a227b75addeae9251cf8acbc7
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2018-01-31 14:46:59 +0100

    Création de vueConnexion et vueInscription

---
+++ app/src/main/java/canadiens/resto/VueConnexion.java
@@ -0,0 +1,13 @@
+package canadiens.resto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VueConnexion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_connexion);
+    }
+}
---
+++ app/src/main/java/canadiens/resto/VueInscription.java
@@ -0,0 +1,13 @@
+package canadiens.resto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VueInscription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_inscription);
+    }
+}
