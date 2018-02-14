fb9153ae552237635a80dc52b5880a63bf77b40e
Frelin Maxime <frelinmaxime@gmail.com>
2018-02-14 15:08:31 +0100

    Ajout de permissions pour la localisation

--- app/src/main/java/canadiens/resto/baseDeDonnees/GoogleMap.java
+++ app/src/main/java/canadiens/resto/baseDeDonnees/GoogleMap.java
@@ -7,9 +7,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -110,12 +112,12 @@ public class GoogleMap extends Fragment implements ActivityCompat.OnRequestPermi
     }
 
     public void demanderPermissionLocalisation(com.google.android.gms.maps.GoogleMap googleMap) {
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            return;
-        }else{
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            //googleMap.setMyLocationEnabled(true);
+        } else {
+            Toast.makeText(getContext(), "Veuillez Activez la localisation", Toast.LENGTH_LONG);
         }
-        googleMap.setMyLocationEnabled(true);
     }
 
     /**
