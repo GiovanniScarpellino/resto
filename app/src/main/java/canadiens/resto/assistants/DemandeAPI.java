5b2029adad20c414b8afc17d426650fd5824a2cf
ppenelon <penelon.pierre@gmail.com>
2018-02-19 00:51:22 +0100

    Ajout des nouvelles requetes dans l'assistant DemandeAPI

--- app/src/main/java/canadiens/resto/assistants/DemandeAPI.java
+++ app/src/main/java/canadiens/resto/assistants/DemandeAPI.java
@@ -8,8 +8,14 @@ public class DemandeAPI {
 
     public static final String API_URL = "http://localhost";
 
-    public static final String REQUETE_CONNEXION_CLIENT = "ConnexionClient";
+    public static final String REQUETE_CONNEXION = "Connexion";
     public static final String REQUETE_INSCRIPTION_CLIENT = "InscriptionClient";
+    public static final String REQUETE_MODIFICATION_CLIENT = "ModificationClient";
+    public static final String REQUETE_RESERVATIONS_CLIENT = "ReservationsClient";
+    public static final String REQUETE_RESTAURANTS_PROCHES = "RestaurantsProches";
+    public static final String REQUETE_MODIFICATION_RESTAURANT = "ModificationRestaurant";
+    public static final String REQUETE_DETAILS_RESTAURANT = "DetailsRestaurant";
+    public static final String REQUETE_RESERVATIONS_RESTAURANT = "ReservationsRestaurant";
 
     public static String genererURL(String nomRequete, String parametres){
         try{
