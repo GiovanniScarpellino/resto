02a8a0aaad0dd01e33c493461a56f13e3975b7b8
ppenelon <penelon.pierre@gmail.com>
2018-02-19 23:39:03 +0100

    Ajout de Reservation comme TypeRequete

--- app/src/main/java/canadiens/resto/api/TypeRequeteAPI.java
+++ app/src/main/java/canadiens/resto/api/TypeRequeteAPI.java
@@ -2,12 +2,13 @@ package canadiens.resto.api;
 
 public class TypeRequeteAPI {
     public static final String CONNEXION = "Connexion";
+    public static final String RESTAURANTS_PROCHES = "RestaurantsProches";
+    public static final String DETAILS_RESTAURANT = "DetailsRestaurant";
+    public static final String RESERVATION = "Reservation";
     public static final String INSCRIPTION_CLIENT = "InscriptionClient";
     public static final String INSCRIPTION_RESTAURANT = "InscriptionRestaurant";
     public static final String MODIFICATION_CLIENT = "ModificationClient";
-    public static final String RESERVATIONS_CLIENT = "ReservationsClient";
-    public static final String RESTAURANTS_PROCHES = "RestaurantsProches";
     public static final String MODIFICATION_RESTAURANT = "ModificationRestaurant";
-    public static final String DETAILS_RESTAURANT = "DetailsRestaurant";
+    public static final String RESERVATIONS_CLIENT = "ReservationsClient";
     public static final String RESERVATIONS_RESTAURANT = "ReservationsRestaurant";
 }
