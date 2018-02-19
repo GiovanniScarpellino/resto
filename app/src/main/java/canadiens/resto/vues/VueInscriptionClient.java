6c1ea2804f73034e95df666328e938326678fa5e
lucas <lucas.paulin@hotmail.fr>
2018-02-19 20:15:48 +0100

    Ajout de la vue de l'inscription client
    Pas encore fonctionnel

--- app/src/main/java/canadiens/resto/vues/VueInscriptionClient.java
+++ app/src/main/java/canadiens/resto/vues/VueInscriptionClient.java
@@ -2,6 +2,9 @@ package canadiens.resto.vues;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.widget.Button;
+import android.widget.EditText;
 
 import canadiens.resto.R;
 
@@ -11,9 +14,30 @@ import canadiens.resto.R;
 
 public class VueInscriptionClient extends AppCompatActivity {
 
+    protected EditText champNom;
+    protected EditText champPrenom;
+    protected EditText champTelephone;
+    protected EditText champMail;
+    protected EditText champMDP;
+
+    protected Button boutonValider;
+    protected Button boutonRetour;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vue_inscription_client);
+
+        champNom = (EditText) findViewById(R.id.champ_nom_client);
+        champPrenom = (EditText) findViewById(R.id.champ_prenom_client);
+        champTelephone = (EditText) findViewById(R.id.champ_tel_client);
+        champMail = (EditText) findViewById(R.id.champ_mail_client);
+        champMDP = (EditText) findViewById(R.id.champ_mdp_client);
+
+        boutonValider = (Button) findViewById(R.id.bouton_valider_client);
+        boutonRetour = (Button) findViewById(R.id.bouton_retour_client);
+
+
+
     }
 }
