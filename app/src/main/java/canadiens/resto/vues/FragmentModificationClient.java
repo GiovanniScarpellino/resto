3b476489ece6efa317366eeeb148c34b66f9b81c
lucas <lucas.paulin@hotmail.fr>
2018-02-22 12:00:59 +0100

    Création du fragment de modification du profil du client

---
+++ app/src/main/java/canadiens/resto/vues/FragmentModificationClient.java
@@ -0,0 +1,34 @@
+package canadiens.resto.vues;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import canadiens.resto.R;
+
+public class FragmentModificationClient extends Fragment {
+
+
+
+    public FragmentModificationClient() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_modification_client, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        
+    }
+}
