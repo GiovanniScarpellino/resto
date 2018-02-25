e6b137235d0ed5ee00ba6a66ea98e43ef1ac7b55
GiovanniScarpellino <31245981+GiovanniScarpellino@users.noreply.github.com>
2018-02-25 15:43:25 +0100

    Correction du l'import du fragment pour le fragmentReservationsClient

--- app/src/main/java/canadiens/resto/vues/FragmentReservationsClient.java
+++ app/src/main/java/canadiens/resto/vues/FragmentReservationsClient.java
@@ -1,10 +1,10 @@
 package canadiens.resto.vues;
 
-import android.app.Fragment;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,10 +26,6 @@ import canadiens.resto.api.ActionsResultatAPI;
 import canadiens.resto.api.RequeteAPI;
 import canadiens.resto.api.TypeRequeteAPI;
 import canadiens.resto.assistants.Token;
-
-/**
- * Created by nicog on 20/02/2018.
- */
 
 public class FragmentReservationsClient extends Fragment {
     private String token;
