2319aa28b5c0e8f048c29fd6f81c4719a1e3327d
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2018-03-10 21:45:36 +0100

    Correction du chargement dialogue

--- app/src/main/java/canadiens/resto/vues/VueConnexion.java
+++ app/src/main/java/canadiens/resto/vues/VueConnexion.java
@@ -44,7 +44,6 @@ public class VueConnexion extends AppCompatActivity {
         new Intent(VueConnexion.this, VuePrincipaleClient.class);
 
         chargementDialogue = new ChargementDialogue(this);
-        chargementDialogue.show();
     }
 
     /**
