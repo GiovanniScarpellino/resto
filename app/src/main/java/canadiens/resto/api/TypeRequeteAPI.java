102cf6735610dcfe1d95129d575db43f81d7fb2a
ppenelon <penelon.pierre@gmail.com>
2018-03-16 21:03:41 +0100

    Ajout des TypeRequeteAPI RecuperationClient et RecuperationRestaurant

--- app/src/main/java/canadiens/resto/api/TypeRequeteAPI.java
+++ app/src/main/java/canadiens/resto/api/TypeRequeteAPI.java
@@ -10,6 +10,8 @@ public class TypeRequeteAPI {
     public static final String INSCRIPTION_RESTAURANT = "InscriptionRestaurant";
     public static final String MODIFICATION_CLIENT = "ModificationClient";
     public static final String MODIFICATION_RESTAURANT = "ModificationRestaurant";
+    public static final String RECUPERATION_CLIENT = "RecuperationClient";
+    public static final String RECUPERATION_RESTAURANT = "RecuperationRestaurant";
     public static final String RESERVATIONS_CLIENT = "ReservationsClient";
     public static final String RESERVATIONS_RESTAURANT = "ReservationsRestaurant";
     public static final String RECUPERATION_CODE_FIDELITE = "RecuperationCodeFidelite";
